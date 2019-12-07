2e7b486e2367fbc83e6fdf45a9af72466667e6d3
Andrew <shevtsov.ap@gmail.com>
2019-12-07 22:40:27 +0100

    add tableview, logo, searchbar

---
+++ MondiamediaTestMusicAPI/Source/UI/MusicItemsViewController/MusicItemsViewController+UISearchResultsUpdating.swift
@@ -0,0 +1,15 @@
+//
+//  MusicItemsViewController+UISearchResultsUpdating.swift
+//  MondiamediaTestMusicAPI
+//
+//  Created by ASH on 07.12.2019.
+//  Copyright © 2019 ASH. All rights reserved.
+//
+
+import UIKit
+
+extension MusicItemsViewController: UISearchResultsUpdating {
+    func updateSearchResults(for searchController: UISearchController) {
+        
+    }
+}
--- MondiamediaTestMusicAPI/Source/UI/MusicItemsViewController/MusicItemsViewController.swift
+++ MondiamediaTestMusicAPI/Source/UI/MusicItemsViewController/MusicItemsViewController.swift
@@ -10,21 +10,22 @@ import UIKit
 
 class MusicItemsViewController: UIViewController {
 
+    let searchController = UISearchController(searchResultsController: nil)
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        setupSearchController()
         // Do any additional setup after loading the view.
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    //MARK: - Private
+    
+    private func setupSearchController() {
+        navigationItem.searchController = searchController
+        searchController.searchResultsUpdater = self
+//        searchController.obscuresBackgroundDuringPresentation = false
+        searchController.searchBar.placeholder = "Search music"
+//        definesPresentationContext = true
     }
-    */
 
 }
